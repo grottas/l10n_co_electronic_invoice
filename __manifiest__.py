3a19f7a08cecd3fd22cc30aa73bf7f9dcfd0858b
svazquezco <svazquez@netquest.com>
2018-12-12 17:42:49 +0100

    [ADD] Added skeleton

---
+++ __manifiest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright <2018> <svazquez@netquest.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Colombia Electronic Invoice',
+    'summary': """Electronic invoice for Colombia localization""",
+    'description': """Electronic invoice for Colombia localization""",
+    'version': '11.0.1.0.0',
+    'category': 'account',
+    'author': 'Netquest',
+    'license': 'AGPL-3',
+    'website': 'http://www.netquest.com',
+    'contributors': [
+        'Susana Vázquez <svazquez@netquest.com>',
+    ],
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'installable': True
+}
