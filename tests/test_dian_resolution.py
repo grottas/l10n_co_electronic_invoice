0229d83d106030a07d540245c705958620ba2c66
svazquezco <svazquez@netquest.com>
2018-12-13 14:00:34 +0100

    [ADD] Added l10n_co_dian_resolution module

---
+++ tests/test_dian_resolution.py
@@ -0,0 +1,28 @@
+from odoo import fields
+
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
+
+
+class TestL10nCoDianResolution(TransactionCase):
+
+    def setUp(self):
+        self.dian_resolution = self.env.ref('l10n.co.dian.resolution')
+        self.resolution_fe = self.dian_resolution.create({
+            'name': 'Resolution FE',
+            'document_type': 'fe',
+            'technical_key': 'abc123455',
+            'prefix': 'PRE',
+            'range_from': '1',
+            'range_to': '100',
+            'next_number': 1,
+            'start_date': fields.Date.today(),
+            'end_date': fields.Date.today(),
+        })
+
+
+
+
+
+
+
